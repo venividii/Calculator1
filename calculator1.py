7a7134fd7f72c4ff1aa9073ffa43edea45d6723a
“Jose <claritooe@gmail.com>
2024-10-30 00:04:59 +0800

    add addition function

--- calculator1.py
+++ calculator1.py
@@ -1,3 +1,6 @@
+def add(x, y):
+
+
 def calculator():
     print("Select operation:")
     print("1. Add")
@@ -7,12 +10,19 @@ def calculator():
 
     while True:
         choice = input("Enter choice (1/2/3/4): ")
-  	if choice == '1':
+        if choice in ('1', '2', '3', '4'):
+            try:
+                num1 = float(input("Enter first number: "))
+                num2 = float(input("Enter second number: "))
+            except ValueError:
+                print("Invalid input! Please enter numbers only.")
+                continue
+            if choice == '1':
                 print(f"{num1} + {num2} = {add(num1, num2)}")
 
-        next_calc = input("Do you want to perform another calculation? (yes/no): ")
-        if next_calc.lower() != 'yes':
-            break
+       	    next_calc = input("Do you want to perform another calculation? (yes/no): ")
+            if next_calc.lower() != 'yes':
+                break
 
 if __name__ == "__main__":
     calculator()
