67df9c8ef68c4d0dbadbb105f664355e859b417d
JL Pendon <155886020+DominuBoi@users.noreply.github.com>
2024-10-30 01:27:20 +0800

    more minor bug fixes 2

--- calculator1.py
+++ calculator1.py
@@ -32,7 +32,7 @@ def calculator():
                 print(f"{num1} + {num2} = {add(num1, num2)}")
             elif choice == '2':
                 print(f"{num1} - {num2} = {subtract(num1, num2)}")
-	        elif choice == '3':
+			elif choice == '3':
                 print(f"{num1} * {num2} = {multiply(num1, num2)}")
             elif choice == '4':
                 print(f"{num1} / {num2} = {divide(num1, num2)}")
