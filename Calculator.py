be61d4c7062674372f99b1db24f6b212dad2e3a4
venividii <89732921+venividii@users.noreply.github.com>
2024-10-29 14:36:31 +0800

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,17 @@
+def calculator():
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
 
+    while True:
+        choice = input("Enter choice (1/2/3/4): ")
+
+
+        next_calc = input("Do you want to perform another calculation? (yes/no): ")
+        if next_calc.lower() != 'yes':
+            break
+
+if __name__ == "__main__":
+    calculator()
